d553e8444f2921474edcfff0aab03d1fe308edbc
Sonali Shakya <sonalishakya@Admins-MacBook-Air.local>
2024-12-12 12:48:59 +0530

    Rebuild

--- yugabyte-integration/src/main/java/com/ondc/yugabyte_integration/Entity/Payload.java
+++ yugabyte-integration/src/main/java/com/ondc/yugabyte_integration/Entity/Payload.java
@@ -1,6 +1,5 @@
 package com.ondc.yugabyte_integration.Entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.ondc.yugabyte_integration.Service.MapToJsonConverter;
 import jakarta.persistence.*;
