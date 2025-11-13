7abddbbefa3c8bdcb2132b7d54ed83ae7f9b4357
extedcouD <extedcoud@gmail.com>
2025-11-13 12:07:20 +0530

    fix: add pagination to stored payloads endpoint

--- src/routes/PayloadRoutes.ts
+++ src/routes/PayloadRoutes.ts
@@ -22,6 +22,6 @@ router.delete("/:id", deletePayload);
 router.get("/transaction/:transactionId", getPayloadByTransactionId);
 router.get("/logs/:transactionId", getPayloadsByTransactionId);
 router.post("/ids", getPayloadByPayloadIds);
-router.get("/stored/:domain/:version", getPayloadsByDomainAndVersion);
+router.get("/stored/:domain/:version/:page", getPayloadsByDomainAndVersion);
 
 export default router;
